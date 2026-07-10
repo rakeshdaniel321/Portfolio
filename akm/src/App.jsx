7ccb85ca1574086bc2dafb0dbbf3da2341096397
Rakesh Daniel <rakeeshdaniel321@gmail.com>
2026-07-10 19:00:25 +0530

    fix:portfolio sumall update

--- akm/src/App.jsx
+++ akm/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import { ArrowLeft } from 'lucide-react';
 import axios from 'axios';
